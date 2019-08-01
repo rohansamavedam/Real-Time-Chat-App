19465d8fbcf12145117238b2a92749a510bc94dc
Rohan Samavedam <42583450+rohansamavedam@users.noreply.github.com>
2019-08-02 00:53:06 +0530

    heroku ready

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
 const { generateMessage } = require('../src/util/messages')
+const { addUser, getUser, getUsersInRoom, removeUser } = require('../src/util/users')
 
 const app = express()
 const server = http.createServer(app) //We created this for the purpose of passing server into socket.io
 const io = socketio(server)
 
-const port = 3000 || process.env.PORT
+const port = process.env.PORT
 
 const publicDir = path.join(__dirname, '../public')
 
@@ -27,33 +28,56 @@ io.on('connection', (socket) => {
 
     const welcomeMessage = 'Welcome!'
 
-    socket.on('join', ({ username, room }) => {
-        socket.join(room)
+    socket.on('join', ({ username, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, username, room })
 
-        socket.emit('message', generateMessage(welcomeMessage))
-        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined the chat.`))
+        if(error){
+            return callback(error)
+        }
 
+        socket.join(user.room)
+
+        socket.emit('message', generateMessage('admin', welcomeMessage))
+        socket.broadcast.to(user.room).emit('message', generateMessage('admin', `${user.username} has joined the chat.`))
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+
+        })
+
+        callback()
 
     })
 
     socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
         const filter = new Filter()
         if(filter.isProfane(message)){
             return callback('Profanity is not allowed')
         }
-        io.emit('message', generateMessage(message))
+        io.to(user.room).emit('message', generateMessage(user.username, message))
         callback()
 
     })
 
     socket.on('sendLocation', (location, callback) => {
+        const user = getUser(socket.id)
         const message = 'https://google.com/maps?q=' + location.lat + ',' + location.long
-        io.emit('locationMessage', generateMessage(message))
+        io.to(user.room).emit('locationMessage', generateMessage(user.username, message))
         callback('Location Shared')
     })
 
     socket.on('disconnect', () => {
-        io.emit('message', generateMessage('A user has left the chat room'))
+        const user = removeUser(socket.id)
+
+        if(user){
+            io.to(user.room).emit('message', generateMessage('admin', `${user.username} has left`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+    
+            })
+        }   
     })
 
 })
